953ee5ebcb963341d5c65c1e2b3f3554adafc7cd
Jot Hogben <jot_hogben@hotmail.co.uk>
2024-08-27 02:49:19 +0100

    Javascript - Adjusting appendItem function with aria labels for buttons

--- script.js
+++ script.js
@@ -18,13 +18,12 @@ const appendItem = (element) => {
   card.innerHTML = `
     <img src="./images/icon-${cardTitle}.svg" alt="" class="card-background-img"/>
     <div class="card__content">
-      <button class="card__ellipsis">
+      <button class="card__ellipsis" aria-label="expand details">
         <svg width="21" height="5" viewBox="0 0 21 5" fill="none" xmlns="http://www.w3.org/2000/svg">
           <circle cx="2.5" cy="2.5" r="2.5" fill="#BBC0FF"/>
           <circle cx="10.5" cy="2.5" r="2.5" fill="#BBC0FF"/>
           <circle cx="18.5" cy="2.5" r="2.5" fill="#BBC0FF"/>
         </svg>
-
       </button>
       <h3 class="card__title">${element.title}</h3>
       <p class="current-hours"></p>
